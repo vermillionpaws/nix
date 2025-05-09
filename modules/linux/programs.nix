201409b057657c16944d7acb5f0e82269ba171b0
vermillionpaws <dev@fault.wtf>
2025-05-08 22:02:11 -0300

    Test formatting

--- modules/linux/programs.nix
+++ modules/linux/programs.nix
@@ -1,68 +1,71 @@
-{ pkgs, ... } : { 
-	environment.systemPackages = with pkgs; [
-		grim
-		pre-commit
-		nixfmt-rfc-style
-		slurp
-		wl-clipboard
-		brightnessctl
-		mako
-		kitty
-		fuzzel
-		sway
-		neovim
-		cmake
-		ninja
-		git
-		fzf
-		fd
-		ripgrep
-		meson
-		luajitPackages.luarocks
-		tree-sitter
-		ffmpeg-full
-		(chromium.override {
-      			commandLineArgs = [
-				"--ignore-gpu-blocklist"
-				"--enable-zero-copy"
-				"--ozone-platform=wayland"
-				"--enable-features=VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE,TouchpadOverscrollHistoryNavigation"
-				"--gtk-version=4"
-				"--enable-wayland-ime"
-				"--wayland-text-input-version=3"
-				"--password-store=gnome-libsecret"
-      			];
-    		})
-	];
-	services.gnome.gnome-keyring = {
-		enable = true;
-	};
-	programs = { 
-		zsh = {
-			enable = true;
-			enableCompletion = true;
-			enableGlobalCompInit = true;
-			autosuggestions = { 
-				enable = true;
-				async = true;
-				strategy = [ "completion" ]; 
-			};
-			syntaxHighlighting = {
-				enable = true;
-				highlighters = [ "main" ];
-			};
-			zsh-autoenv = {
-				enable = true;
-			};
-			ohMyZsh = {
-				enable = true;
-			};
-		};
-		sway = {
-			enable = true;
-			wrapperFeatures = {
-				gtk = true;
-			};
-		};
-	};
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    grim
+    pre-commit
+    nixfmt-rfc-style
+    cabal-install
+    ghc
+    slurp
+    wl-clipboard
+    brightnessctl
+    mako
+    kitty
+    fuzzel
+    sway
+    neovim
+    cmake
+    ninja
+    git
+    fzf
+    fd
+    ripgrep
+    meson
+    luajitPackages.luarocks
+    tree-sitter
+    ffmpeg-full
+    (chromium.override {
+      commandLineArgs = [
+        "--ignore-gpu-blocklist"
+        "--enable-zero-copy"
+        "--ozone-platform=wayland"
+        "--enable-features=VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE,TouchpadOverscrollHistoryNavigation"
+        "--gtk-version=4"
+        "--enable-wayland-ime"
+        "--wayland-text-input-version=3"
+        "--password-store=gnome-libsecret"
+      ];
+    })
+  ];
+  services.gnome.gnome-keyring = {
+    enable = true;
+  };
+  programs = {
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableGlobalCompInit = true;
+      autosuggestions = {
+        enable = true;
+        async = true;
+        strategy = [ "completion" ];
+      };
+      syntaxHighlighting = {
+        enable = true;
+        highlighters = [ "main" ];
+      };
+      zsh-autoenv = {
+        enable = true;
+      };
+      ohMyZsh = {
+        enable = true;
+      };
+    };
+    sway = {
+      enable = true;
+      wrapperFeatures = {
+        gtk = true;
+      };
+    };
+  };
 }
